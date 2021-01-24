3c597b157846a2ef8a4b40989702d6627e287304
Boris Serdiuk <just-boris@hotmail.com>
2021-01-24 22:13:52 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/",
+  },
+  optimize: {
+    bundle: true,
+    minify: true,
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    out: "build/static",
+    sourcemap: true,
+  },
+};
